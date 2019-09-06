9063f655277bd4de50276f8a1c1bc739a8b64cd7
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-09-06 08:59:58 +0100

    working on tabs component.  Struggling with the functionality of the axios

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function getLambdaTimes() {
+
+  axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+      const topicsList = response.data.topics;
+      console.log(topicsList);
+      topicsList.forEach((info) => {
+        const newtab = Tab(info);
+        console.log(newtab);
+      })
+        // const newtab = Tab(response.data);
+        // const newDiv = document.createElement('div');
+        // newDiv.appendChild(newcard);
+        // const cards = document.querySelector('.cards');
+        // cards.appendChild(newDiv);
+    })
+    .catch(error => {
+        console.log(error.message);
+    });
+}
+getLambdaTimes();
+
+function Tab(info) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.textContent = info;
+  return tab;
+};
